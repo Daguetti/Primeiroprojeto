050ee521c5da697094fc480fead4a065bee947f8
Daguetti <mathdaguetti@hotmail.com>
2018-10-08 11:16:40 -0300

    last commit

--- Priproj/Primeiroprojeto/Primeiroprojeto.cpp
+++ Priproj/Primeiroprojeto/Primeiroprojeto.cpp
@@ -12,7 +12,7 @@ mano
 #include <stdio.h>
 #include <stdlib.h>
 
-//Variáveis (Globais)
+//Variáveis Globais
 int a; //inteiro
 float b; //real (ponto futuante)
 double; //real (maior precisão)
@@ -22,6 +22,39 @@ bool d; //booleano (verdadeio/falso)
 
 int main()
 {
+	//Variável local
+	int e;
+
+	printf("Hello World!\n\n");
+
+	//imprimindo valores
+
+	printf("%i\n\n", 17);
+	printf("%f\n\n", 1.7);
+
+	//texto (string e valor juntos)
+
+	printf("Aqui vai o valor %i, total da soma. \n\n", 10 + 7);
+
+	printf("%i + %i = %i\n\n", 10, 7, 10 + 7);
+
+	a = 10;
+	e = 7;
+	int f = a + e;
+
+	printf("O resultado da soma eh: %i\n\n", f);
+
+	//leitura SCANF/SCANF_S
+
+	printf("Digite um valor bonitinho: ");
+	
+	scanf_s("%i", &f);
+
+	printf("\nVoce digitou %i\n\n", f);
+
+
+	system("pause");
+
     return 0;
 }
 
