a3a2749da831eadf45cd53bfc977a02c2899460b
Daguetti <mathdaguetti@hotmail.com>
2018-10-08 09:34:50 -0300

    Variavél global

--- Priproj/Primeiroprojeto/Primeiroprojeto.cpp
+++ Priproj/Primeiroprojeto/Primeiroprojeto.cpp
@@ -1,7 +1,22 @@
 // Primeiroprojeto.cpp : Defines the entry point for the console application.
 //
 
+/*
+Krai borracha
+mano
+*/
+
+
 #include "stdafx.h"
+//C ANSI
+#include <stdio.h>
+#include <stdlib.h>
+
+//Variáveis (Globais)
+int a; //inteiro
+float b; //real (ponto futuante)
+double; //real (maior precisão)
+bool d; //booleano (verdadeio/falso)
 
 
 
