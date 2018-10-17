bd767c980fedf5cd44779ee6459f9b01f31e5758
Daguetti <mathdaguetti@hotmail.com>
2018-10-17 09:19:42 -0300

    agr sim

--- 1010/CalculadoraSimples/CalculadoraSimples.cpp
+++ 1010/CalculadoraSimples/CalculadoraSimples.cpp
@@ -4,56 +4,30 @@
 #include "stdafx.h"
 #include <stdlib.h>
 
-float Soma(float a, float b)
-{
-	float c;
-	c = a + b;
-	return c;
+float Soma(float a, float b){
+	return a + b;
 }
-
-float Subtracao(float a, float b)
-{
-	float c;
-	c = a - b;
-	return c;
+float Subtracao(float a, float b){
+	return a - b;
 }
-
-float Multiplicacao(float a, float b)
-{
-	float c;
-	c = a * b;
-	return c;
+float Multiplicacao(float a, float b){
+	return a * b;
 }
-
-float Divisao(float a, float b)
-{
-	float c;
-	c = a / b;
-	return c;
+float Divisao(float a, float b){
+	return a / b;
 }
-
-float LendoValores()
-{
+float LendoValores(){
 	float variavel;
 	printf("\nDigite um valor: \n");
 	scanf_s("%f", &variavel);
 	return variavel;
 }
-
-float main()
-{
+float main(){
 	float primeiroValor = LendoValores();
 	float segundoValor = LendoValores();
-
-	float resultado = Soma(primeiroValor, segundoValor);
-	printf("\nResultado da soma dos valores: %f", resultado);
-	resultado = Subtracao(primeiroValor, segundoValor);
-	printf("\nResultado da subtracao dos valores: %f", resultado);
-	resultado = Multiplicacao(primeiroValor, segundoValor);
-	printf("\nResultado da multiplicacao dos valores: %f", resultado);
-	resultado = Divisao(primeiroValor, segundoValor);
-	printf("\nResultado da divisao dos valores: %f\n", resultado);
+	printf("\nResultado da soma dos valores: %f", Soma(primeiroValor, segundoValor));
+	printf("\nResultado da subtracao dos valores: %f", Subtracao(primeiroValor, segundoValor));
+	printf("\nResultado da multiplicacao dos valores: %f", Multiplicacao(primeiroValor, segundoValor));
+	printf("\nResultado da divisao dos valores: %f\n", Divisao(primeiroValor, segundoValor));
 	system("pause");
-    return 0;
 }
-
