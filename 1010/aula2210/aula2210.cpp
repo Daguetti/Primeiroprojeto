7292348df74569ce44f955b8f44eb576f9955ead
Daguetti <mathdaguetti@hotmail.com>
2018-10-22 09:25:57 -0300

    MaiorValorComp

--- 1010/aula2210/aula2210.cpp
+++ 1010/aula2210/aula2210.cpp
@@ -1,48 +1,25 @@
-// aula2210.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 #include <stdlib.h>
-
-int MVI(int a, int b, int c, int d)
-{
+int MVI(int a, int b, int c, int d){
 	int maior = a;
-
-	if (maior < b)
-	{
-		maior = b;
-	}
-	if (maior < c)
-	{
-		maior = c;
-	}
-	if (maior < d)
-	{
-		maior = d;
-	}
-	return maior;
-}
-
-int LendoValores()
-{
+	if (maior < b){
+		maior = b;}
+	if (maior < c){
+		maior = c;}
+	if (maior < d){
+		maior = d;}
+	return maior;}
+int LendoValores(){
 	int valor = 0;
 	printf("\nDigite um numero inteiro: \n");
 	scanf_s("%i", &valor);
-	return valor;
-}
-
-int main()
-{
+	return valor;}
+int main(){
 	int primeiro = LendoValores();
 	int segundo = LendoValores();
 	int terceiro = LendoValores();
 	int quarto = LendoValores();
-	
 	int m = MVI(primeiro, segundo, terceiro, quarto);
-	
 	printf("\nO maior valor encontrado foi: %i\n", m);
-	
 	system("pause");
-    return 0;
-}
-
+    return 0;}
