c37edd06207abdc0b53b9f52cacc71b6983f5eb6
Daguetti <mathdaguetti@hotmail.com>
2018-10-22 11:30:39 -0300

    FinalAula

--- 1010/aula2210/aula2210.cpp
+++ 1010/aula2210/aula2210.cpp
@@ -9,32 +9,23 @@ int MVI(int a, int b, int c, int d){
 	if (maior < d){
 		maior = d;}
 	return maior;}
-
 int LendoValores(){
 	int valor = 0;
 	printf("\nDigite um numero inteiro: \n");
 	scanf_s("%i", &valor);
 	return valor;}
-
-float LVR()
-{
+float LVR(){
 	float valor = 0;
 	printf("\nDigite um numero real (utlilizando ponto): \n");
 	scanf_s("%f", &valor);
-	return valor;
-}
-
+	return valor;}
 float media(float a, float b, float c, float d){
 	float media = (a + b + c + d) / 4;
 	return media;}
-
-void IVMQM(float a, float b, float c, float d)
-{
+void IVMQM(float a, float b, float c, float d){
 	float med = media(a, b, c, d);
-
 	printf("\nO valor da media eh: %.2f\n", med);
 	printf("\nOs valores maiores que a media sao:\n");
-
 	if (a > med){
 		printf("\n%.2f ", a);}
 	if (b > med){
@@ -42,17 +33,13 @@ void IVMQM(float a, float b, float c, float d)
 	if (c > med){
 		printf("\n%.2f ", c);}
 	if (d > med){
-		printf("\n%.2f \n", d);}
-}
-
-void ExemploSwitch()
-{
+		printf("\n%.2f \n", d);}}
+void ExemploSwitch(){
 	printf("\nEscolha uma opcao: \n");
 	printf("1 - a\n2 - b\n3 - c\n4 - d\n");
 	int op = 0;
 	scanf_s("%i", &op);
-	switch (op)
-	{
+	switch (op){
 	case 1:
 		printf("Voce escolheu a\n");
 		break;
@@ -67,10 +54,7 @@ void ExemploSwitch()
 		break;
 	default:
 		printf("opcao invalida\n");
-		break;
-	}
-}
-
+		break;}}
 int main(){
 	/*int primeiro = LendoValores();
 	int segundo = LendoValores();
@@ -78,14 +62,10 @@ int main(){
 	int quarto = LendoValores();
 	int m = MVI(primeiro, segundo, terceiro, quarto);
 	printf("\nO maior valor encontrado foi: %i\n", m);
-
 	float primeiroReal = LVR();
 	float segundoReal = LVR();
 	float terceiroReal = LVR();
 	float quartoReal = LVR();
-
 	IVMQM(primeiroReal, segundoReal, terceiroReal, quartoReal);*/
 	ExemploSwitch();
-
-	system("pause");
-    return 0;}
+	system("pause");}
